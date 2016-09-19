cc8ce9589aea6781f73d9ef213a7fb0991a9d2c1
Russell Brooks <Russellc.brooks@gmail.com>
2016-09-19 13:02:14 -0500

    FizzBuzz

---
+++ fizzbuzz/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fizzbuzz
+{
+    class Program
+    {   //Write a program that prints the numbers from 1 to 100. But for multiples of three print "Fizz" instead of the number and for the multiples of five print "Buzz". For numbers which are multiples of both three and five print "FizzBuzz".
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {   // Also could be if (i % 15 == 0)
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                    continue;
+                }
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                    continue;
+                }
+                if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                    continue;
+                }
+                Console.WriteLine(i);
+                
+            }
+            Console.ReadLine();
+        }
+    }
+}
